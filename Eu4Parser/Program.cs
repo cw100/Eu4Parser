933803cbec9a4e19294440412508028701225881
Connor Wegrzyn <20263958@eastridingcollege.ac.uk>
2018-06-25 20:03:18 +0100

    Initial province loading done

--- Eu4Parser/Program.cs
+++ Eu4Parser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,134 @@ namespace Eu4Parser
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            List<Province> provinces = new List<Province>();
+            provinces = LoadProvinces(@"E:\Steam\SteamApps\common\Europa Universalis IV\history\provinces");
+            provinces = provinces.OrderBy(o => o.id).ToList();
+            foreach (Province province in provinces)
+            {
+                province.Print();
+            }
+            Console.Read();
         }
         public static string LoadFile(string path)
         {
             string result;
             result = System.IO.File.ReadAllText(path);
             return result;
+        }
+
+        public static List<Province> LoadProvinces(string path)
+        {
+            List<Province> provinces = new List<Province>();
+            foreach (string file in Directory.EnumerateFiles(path, "*.txt"))
+            {
+                string owner="";
+                string controller="";
+                string religion = "";
+                string culture = "";
+                string tradeGood = "";
+                string capital = "";
+                bool centerOfTrade = false;
+                bool city = false;
+                bool hre = false;
+                int tax= 0;
+                int production=0;
+                int manpower=0;
+                int id=0;
+                bool coal = false;
+                try
+                {
+                    id = int.Parse(Path.GetFileName(file).Split(' ')[0]);
+                 
+                }
+                catch
+                {
+                    try
+                    {
+                        id = int.Parse(Path.GetFileName(file).Split('-')[0]);
+                       
+                    }
+                    catch
+                    {
+                    }
+                    }
+                string[] lines = File.ReadAllLines(file);
+                foreach (string line in lines)
+                {
+                    string[] statement = line.Split(' ');
+                    switch (statement[0])
+                    {
+                        case "owner":
+                            owner = statement[2].Substring(0,3);
+                            break;
+                        case "controller ":
+                            controller = statement[2].Substring(0, 3);
+                            break;
+                        case "culture":
+                            culture = statement[2];
+                            break;
+                        case "religion":
+                            religion = statement[2];
+                            break;
+                        case "capital":
+                            capital = statement[2];
+                            break;
+                        case "trade_goods":
+                            tradeGood = statement[2];
+                            break;
+                        case "hre":
+                          if(statement[2]=="yes")
+                                hre=true;
+                            break;
+                        case "base_tax":
+                            try
+                            {
+                                tax = int.Parse(statement[2]);
+                            }
+                            catch
+                            {
+
+                            }
+                            break;
+                        case "base_production":
+                            try
+                            {
+                                production = int.Parse(statement[2]);
+                            }
+                            catch
+                            {
+
+                            }
+                            break;
+                        case "base_manpower":
+                            try
+                            {
+                                manpower = int.Parse(statement[2]);
+                            }
+                            catch
+                            {
+
+                            }
+                            break;
+                        case "is_city":
+                            if (statement[2] == "yes")
+                                city = true;
+                            break;
+                        case "	name":
+                            if (statement[2] == "center_of_trade_modifier")
+                                centerOfTrade = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Province province = new Province(id,owner,controller,capital,tradeGood,religion,culture,tax,production,manpower,centerOfTrade,city,hre,coal);
+                provinces.Add(province);
+            }
+            return provinces;
         }
     }
 }
--- Eu4Parser/Province.cs
+++ Eu4Parser/Province.cs
@@ -11,20 +11,47 @@ namespace Eu4Parser
         public string owner;
         public string controller;
         public string religion;
+        public string culture;
         public string tradeGood;
         public string capital;
         public bool centerOfTrade;
         public bool city;
         public bool hre;
-        int tax;
-        int production;
-        int manpower;
-        int id;
-        public Province(int id, string owner, string controller, string captial, string tradeGood, string religion, 
+        public int tax;
+        public int production;
+        public int manpower;
+        public int development;
+        public int id;
+        public bool coal;
+        
+        public Province(int id, string owner, string controller, string capital, string tradeGood, string religion , string culture, 
             int tax, int production, int manpower,
-            bool centerOfTrade, bool city, bool hre)
+            bool centerOfTrade, bool city, bool hre, bool coal)
         {
             this.id = id;
+            this.owner = owner;
+            this.controller = controller;
+            this.capital = capital;
+            this.tradeGood = tradeGood;
+            this.religion = religion;
+            this.culture = culture;
+            this.tax = tax;
+            this.production = production;
+            this.manpower = manpower;
+
+            development = tax + production + manpower;
+
+            this.centerOfTrade = centerOfTrade;
+            this.city = city;
+            this.hre = hre;
+            this.coal = coal;
+
         }
+        public void Print()
+        {
+            Console.WriteLine(id + "-" + owner + " " + capital + " Development: " + development + "(" + tax + ", " + production + ", " + manpower + ")" +
+                " Culture: " + culture + " Religion: " + religion + " Trade Good: " + tradeGood + " Has coal: " + coal + " In hre: " + hre +
+                " Is a city: " + city + " Is a center of trade: " + centerOfTrade);
+        } 
     }
 }
