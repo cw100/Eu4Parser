222de68654d52c7abefacae59949aaf829564c28
Connor Wegrzyn <20263958@eastridingcollege.ac.uk>
2018-06-25 19:03:08 +0100

    Add project files.

---
+++ Eu4Parser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eu4Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static string LoadFile(string path)
+        {
+            string result;
+            result = System.IO.File.ReadAllText(path);
+            return result;
+        }
+    }
+}
---
+++ Eu4Parser/Province.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eu4Parser
+{
+    class Province
+    {
+        public string owner;
+        public string controller;
+        public string religion;
+        public string tradeGood;
+        public string capital;
+        public bool centerOfTrade;
+        public bool city;
+        public bool hre;
+        int tax;
+        int production;
+        int manpower;
+        int id;
+        public Province(int id, string owner, string controller, string captial, string tradeGood, string religion, 
+            int tax, int production, int manpower,
+            bool centerOfTrade, bool city, bool hre)
+        {
+            this.id = id;
+        }
+    }
+}
